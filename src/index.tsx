dc0cf409e8dc8e77438198e7a3d73c981296d243
KiKiKi-KiKi <chaika.design@gmail.com>
2021-10-22 18:50:14 +0900

    Use Recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import { ReactKeycloakProvider } from '@react-keycloak/web';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { keycloak } from './keycloak';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { RecoilRoot } from 'recoil';
+import { Observer } from './Observer';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactKeycloakProvider authClient={keycloak}>
-      <App />
-    </ReactKeycloakProvider>
+    <RecoilRoot>
+      <ReactKeycloakProvider authClient={keycloak}>
+        <Observer />
+        <App />
+      </ReactKeycloakProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
