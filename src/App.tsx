14b9157229840eb846d09d8d9c0a5335a218436e
KiKiKi-KiKi <chaika.design@gmail.com>
2021-10-22 17:13:51 +0900

    Login & Logout by Keycloak

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import { useKeycloak } from '@react-keycloak/web';
+import React, { useCallback } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello, Keycloak App</div>;
+  const { keycloak, initialized } = useKeycloak();
+  const isAuthenticated = !!keycloak.authenticated;
+
+  const handleLogin = useCallback(() => {
+    keycloak.login();
+  }, [keycloak]);
+
+  const handleLogout = useCallback(() => {
+    keycloak.logout();
+  }, [keycloak]);
+
+  console.log({ auth: keycloak.authenticated });
+  console.log({ keycloak, initialized });
+
+  return (
+    <div className="App">
+      <h1>Hello, Keycloak App</h1>
+      <div>
+        <div>
+          <p>{`User is ${!isAuthenticated ? 'NOT ' : ''}authenticated`}</p>
+        </div>
+        {isAuthenticated ? (
+          <button type="button" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <>
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                keycloak.register();
+              }}
+            >
+              Sign up
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
